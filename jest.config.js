db527f332727a792470f71d7c4f7e9f43d5d4618
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-06-13 23:44:34 +0300

    add changes

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
 /** @type {import('jest').Config} */
 const config = {
-    coverageThreshold: {
-      global: {
-        branches: 100,
-        functions: 100,
-        lines: 100,
-      },
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  "coverageThreshold": {
+    global: {
+    "branches": 100,
+    "functions": 100,
+    "lines": 100
     },
-  };
-  
-  module.exports = config;
+  },
+};
+
+module.exports = config;
