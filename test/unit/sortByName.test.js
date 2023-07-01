911f4f821527a80422d7280ec0442884a5ecb90c
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-07-01 12:32:28 +0300

    tests modified

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -15,3 +15,51 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order2", () => {
+    expect(
+      sorting.sortByName([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order3", () => {
+    expect(
+      sorting.sortByName([
+        "Волшебник изумрудного города",
+        "Гарри Поттер",
+        "Властелин Колец",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order4", () => {
+    expect(
+      sorting.sortByName([
+        "Властелин Колец",
+        "Гарри Поттер",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
